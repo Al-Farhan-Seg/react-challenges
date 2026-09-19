8b0b9cc54aeb6c282b2cd8a085c4a1fb030382dc
Al-Farhan-Seg <farhan.segujja@gmail.com>
2026-09-19 07:46:29 +0300

    Added Vercel Workflow

--- dev-challenges/01-simple-coffee-listing/vite.config.js
+++ dev-challenges/01-simple-coffee-listing/vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  base: '/dev-challenges/01-simple-coffee-listing',
+export default defineConfig(({ mode }) => ({
+  base:
+    mode === 'vercel'
+      ? '/'
+      : '/dev-challenges/01-simple-coffee-listing/',
   plugins: [react(), tailwindcss()],
-})
+}))
